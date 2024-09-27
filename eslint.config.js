565e1e06d2db43db0c5aa3a695f05c54cd81c616
Greg Johnston <gregmj.sco@gmail.com>
2024-09-27 18:08:18 +0900

    Initial commit

---
+++ eslint.config.js
@@ -0,0 +1,25 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+
+export default [
+  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
+  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
+  { languageOptions: { globals: globals.browser } },
+  {
+    rules: {
+      eqeqeq: "off",
+      "no-unused-vars": "error",
+      "prefer-const": ["error", { ignoreReadBeforeAssign: true }],
+    },
+  },
+  {
+    ignores: [".node_modules/*"],
+  },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  eslintPluginPrettierRecommended,
+];
---
+++ src/index.js
@@ -0,0 +1,5 @@
+// src/index.ts
+var favoriteFruits = ["apple", "strawberry", "orange"];
+function addFruit(fruit) {
+    favoriteFruits.push(fruit);
+}
